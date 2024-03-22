ff769f2af13a036325ee57a3a3de7e542871d5f5
SLINCOLNS <91050056+SLINCOLNS@users.noreply.github.com>
2024-03-22 22:58:03 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,20 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
     var modal = document.getElementById('modal');
-    var modalOverlay = document.getElementById('modal-overlay'); // Получаем размытый фон
-
+    var modalOverlay = document.getElementById('modal-overlay'); 
     modal.style.display = 'block';
-    modalOverlay.style.display = 'block'; // Отображаем размытый фон
+    modalOverlay.style.display = 'block'; 
 
     // Close the modal and modal overlay when clicking on the close button
     modal.querySelector('.close').addEventListener('click', function() {
         modal.style.display = 'none';
-        modalOverlay.style.display = 'none'; // Скрываем размытый фон
+        modalOverlay.style.display = 'none'; 
     });
 
     // Close the modal and modal overlay when clicking outside of the modal
     window.addEventListener('click', function(event) {
         if (event.target == modal) {
             modal.style.display = 'none';
-            modalOverlay.style.display = 'none'; // Скрываем размытый фон
+            modalOverlay.style.display = 'none';
         }
     });
 });
