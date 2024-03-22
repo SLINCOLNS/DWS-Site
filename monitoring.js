27745f6ffc62871c5811e86372b7f1ebd04f45b7
SLINCOLNS <91050056+SLINCOLNS@users.noreply.github.com>
2024-03-22 22:53:48 +0200

    Update monitoring.js

--- monitoring.js
+++ monitoring.js
@@ -1,10 +1,8 @@
 const serverIDs = ['17433026', '23324021', '24287959'];
 const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImNjNjU5YWUwMTYzMDlmOTIiLCJpYXQiOjE3MDgxMjk4MDcsIm5iZiI6MTcwODEyOTgwNywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo4MjQwMjYifQ.X3C-We37p4aRS7csZv_PHM9SYG77nSMAmQJ6gVR4b5o';
 
-// Объект для хранения данных о серверах
 const serversData = {};
 
-// Функция для загрузки данных о сервере по его ID
 function loadServerData(serverID) {
     const apiUrl = `https://api.battlemetrics.com/servers/${serverID}`;
 
@@ -26,7 +24,6 @@ function loadServerData(serverID) {
     .catch(error => console.error('Error fetching server data:', error));
 }
 
-// Функция для отображения данных о серверах в заданном порядке
 function displayServers() {
     const serverInfoContainer = document.querySelector('.server-info');
     serverInfoContainer.innerHTML = '';
@@ -44,9 +41,9 @@ function displayServers() {
     });
 }
 
-// Загружаем данные о каждом сервере
+
 serverIDs.forEach(serverID => {
     loadServerData(serverID);
 });
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImNjNjU5YWUwMTYzMDlmOTIiLCJpYXQiOjE3MDgxMjk4MDcsIm5iZiI6MTcwODEyOTgwNywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo4MjQwMjYifQ.X3C-We37p4aRS7csZv_PHM9SYG77nSMAmQJ6gVR4b5o
+// мой ip на battlemetricks: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImNjNjU5YWUwMTYzMDlmOTIiLCJpYXQiOjE3MDgxMjk4MDcsIm5iZiI6MTcwODEyOTgwNywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjo4MjQwMjYifQ.X3C-We37p4aRS7csZv_PHM9SYG77nSMAmQJ6gVR4b5o
